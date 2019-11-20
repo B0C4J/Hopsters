f1c517090670d0520257caf737ddf2f68fb2d3b7
Jacob Irving <jacob.irving@accelo.com>
2019-11-20 13:05:37 +1100

    Remove firebase details

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,6 @@ import firebase from 'react-native-firebase';
 
 
 const App = () => {
-	// var Hopsters = firebase.initializeApp({
-	// 	apiKey: "AIzaSyBxx9FU48R38wY2wkYoydMXhSCDGZw4dFA",
-	// 	databaseURL: "https://hopsters-gerringong.firebaseio.com",
-	// 	projectId: "hopsters-gerringong",
-	// 	storageBucket: "hopsters-gerringong.appspot.com",
-	// 	appId: "1:609035491418:web:6331e427499cc1e928a063",
-	// 	messagingSenderId: "609035491418",
-	//   }, "Hopsters");
 	return (
 		<>
 			<Header />
